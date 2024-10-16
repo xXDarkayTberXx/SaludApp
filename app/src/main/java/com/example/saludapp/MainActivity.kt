9d571a1e4e4bdbd14e7268fae8b9d00560148000
xXDarkayTberXx <madalolito22@gmail.com>
2024-10-16 11:40:06 +0200

    1.1 (Logcat button press)

--- app/src/main/java/com/example/saludapp/MainActivity.kt
+++ app/src/main/java/com/example/saludapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.saludapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnHello:AppCompatButton = findViewById(R.id.btnHello)
+        btnHello.setOnClickListener {
+            Log.i("Test", "Boton pulsado")
+        }
     }
 }
