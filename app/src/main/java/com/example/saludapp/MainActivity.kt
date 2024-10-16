0b6f54fbb42509e64d31f9f23c53c89e1525595d
xXDarkayTberXx <madalolito22@gmail.com>
2024-10-16 12:04:35 +0200

    1.2 (Toast button press formatted)

--- app/src/main/java/com/example/saludapp/MainActivity.kt
+++ app/src/main/java/com/example/saludapp/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.saludapp
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 
@@ -13,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnHello:AppCompatButton = findViewById(R.id.btnHello)
+        val btnHello: AppCompatButton = findViewById(R.id.btnHello)
         val etName: AppCompatEditText = findViewById(R.id.etname)
         btnHello.setOnClickListener {
             val name = etName.text.toString()
